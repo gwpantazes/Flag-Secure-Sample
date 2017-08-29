2f6f2583f8a98ecc1f129030e801165681bf74ad
George Pantazes <George.X.Pantazes.-ND@disney.com>
2017-08-29 09:28:02 -0700

    App Not Secure
    This branch has a version of the app that is not secure.

--- app/src/main/java/com/georgepantazes/qualityservices/flagsecuresample/MainActivity.java
+++ app/src/main/java/com/georgepantazes/qualityservices/flagsecuresample/MainActivity.java
@@ -9,11 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,
-                WindowManager.LayoutParams.FLAG_SECURE);
-
-
         setContentView(R.layout.activity_main);
     }
 }
